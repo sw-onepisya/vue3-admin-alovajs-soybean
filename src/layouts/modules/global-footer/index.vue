49192f6efe662a4caaaae09f93aa4063ac9e17cb
onepisYa <pis1@qq.com>
2025-06-10 10:29:15 +0800

    docs: 添加TODO注释用于后续功能开发
    添加关于logo切换、loading自定义和版权信息更改的TODO注释

--- src/layouts/modules/global-footer/index.vue
+++ src/layouts/modules/global-footer/index.vue
@@ -6,6 +6,7 @@ defineOptions({
 
 <template>
   <DarkModeContainer class="h-full flex-center">
+    <!-- TODO: 12 Copyright 更改 -->
     <a href="https://github.com/soybeanjs/soybean-admin/blob/main/LICENSE" target="_blank" rel="noopener noreferrer">
       Copyright MIT © 2021 Soybean
     </a>
